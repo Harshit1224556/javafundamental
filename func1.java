9f86b68202c585647ad55959356e084348d79a35
Harshit1224556 <150434886+Harshit1224556@users.noreply.github.com>
2024-12-30 21:18:18 +0530

    Add files via upload
    this is function program

---
+++ func1.java
@@ -0,0 +1,11 @@
+package Function.function1;
+public class func1 {
+    public static void main(String[] args){
+        greet();
+        greet();
+    }
+
+    static void greet(){
+        System.out.println("good morning");
+    }
+}
