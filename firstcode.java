04310177419b97650806cd75064cd57324c97b58
Harshit1224556 <150434886+Harshit1224556@users.noreply.github.com>
2025-02-24 22:40:40 +0530

    Add files via upload

---
+++ firstcode.java
@@ -0,0 +1,21 @@
+package oops;
+
+class Student{
+    String name;
+    int marks;
+    float percentage;
+Student(){
+    name = "Rahullll";
+}
+
+}
+
+
+public class firstcode {
+    public static  void main(String [] args){
+
+ Student s1 = new Student();
+        System.out.println(s1.name);
+    }
+}
+
